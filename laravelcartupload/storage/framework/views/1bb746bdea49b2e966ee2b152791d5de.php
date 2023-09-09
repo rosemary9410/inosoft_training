c99bb2624f9482f5b11196dc522e3035c3f0ba73
rosemary9410 <141228526+rosemary9410@users.noreply.github.com>
2023-09-10 02:57:34 +0700

    Add files via upload

---
+++ laravelcartupload/storage/framework/views/1bb746bdea49b2e966ee2b152791d5de.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyek Laravel dengan Vue.js dan Bootstrap</title>
+
+    <!-- Include Bootstrap CSS -->
+    <link href="<?php echo e(asset('css/app.css')); ?>" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <!-- Komponen ProductComponent -->
+        <product-component></product-component>
+    </div>
+
+    <!-- Include Bootstrap JavaScript -->
+    <script src="<?php echo e(asset('js/app.js')); ?>" defer></script>
+</body>
+</html>
+<?php /**PATH C:\Users\joane\laravel-proj\laravel-cart\resources\views/welcome.blade.php ENDPATH**/ ?>
